b284f3efdc87542f58de58b4a5e7625accd270f1
Dan Forsberg <dan.s.forsberg@gmail.com>
2021-10-03 14:36:19 +0200

    finished

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
+import RuneWordSearcher from "./RuneWordSearcher.svelte";
+
 </script>
 
-<main />
+<RuneWordSearcher />
 
 <style>
+    :global(body) {
+        background-color: black;
+        color: green;
+    }
+
+    :global(input) {
+        background-color: black;
+        color: green;
+    }
 </style>
